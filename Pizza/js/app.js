c0e9e582c68f5f02c790c9182b35f9acd36a5318
Quinn Lopreato <99095631+qclopreato@users.noreply.github.com>
2022-02-21 19:08:42 -0600

    Update app.js

--- Pizza/js/app.js
+++ Pizza/js/app.js
@@ -11,14 +11,45 @@ let finalSides = document.getElementById(`finalSides`);
 let finalDesserts = document.getElementById(`finalDesserts`);
 let dollars = 0;
 let taxDollars = 0;
-var costPep = 1.75;
-var costSau = 1.50;
-var costMus = 1.00;
-var costFries = 2.00;
-var costBuf = 6.00;
-var costBbq = 6.00;
-var costCherry = 1.50;
-var costMango = 2.00;
+const costPep = 1.75;
+const costSau = 1.50;
+const costMus = 1.00;
+const costFries = 2.00;
+const costBuf = 6.00;
+const costBbq = 6.00;
+const costCherry = 1.50;
+const costMango = 2.00;
+
+document.querySelectorAll(`.carousel`).forEach(carousel => {
+    const items = carousel.querySelectorAll(`.carousel__item`);
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel__button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend", `
+        <div class="carousel__nav">
+            ${ buttonsHtml.join("")}
+        </div>
+    `);
+    
+    const buttons = carousel.querySelectorAll(`.carousel__button`);
+
+    buttons.forEach((button, i) => {
+        button.addEventListener(`click`, () => {
+            //un-select all the items
+            items.forEach(item => item.classList.remove(`carousel__item--selected`));
+            buttons.forEach(button => button.classList.remove(`carousel__button--selected`));
+
+            items[i].classList.add(`carousel__item--selected`);
+            button.classList.add(`carousel__button--selected`);
+        });
+    });
+
+    //Select the first item on the pages
+    items[0].classList.add(`carousel__item--selected`);
+    buttons[0].classList.add(`carousel__button--selected`);
+})
+
 
 function submitToppings(){
     if (pep.checked == true && sau.checked == true && mus.checked == true){
